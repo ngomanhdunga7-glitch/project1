82aa633369f647c966c55362855d8862111ce0bd
Ngo Manh Dung <ngomanhdunga7@gmail.com>
2026-09-01 23:49:31 +0700

    them chuc nang tim kiem va sap xep theo diem

--- quan-ly-sinh-vien/main.cpp
+++ quan-ly-sinh-vien/main.cpp
@@ -5,20 +5,20 @@
 
 using namespace std;
 
-// 1. Định nghĩa cấu trúc 1 Sinh Viên
 struct SinhVien {
     string maSV;
     string hoTen;
     float diemTB;
 };
 
-// Khai báo các hàm xử lý
 void nhapSinhVien(SinhVien &sv);
 void inSinhVien(const SinhVien &sv);
 void hienThiDanhSach(const vector<SinhVien> &ds);
+void timKiemSinhVien(const vector<SinhVien> &ds);
+void sapxep(vector<SinhVien> &ds);
 
 int main() {
-    vector<SinhVien> dsSinhVien; // Danh sách chứa các sinh viên
+    vector<SinhVien> dsSinhVien;
     int luaChon;
 
     do {
@@ -27,6 +27,8 @@ int main() {
         cout << "====================================\n";
         cout << "1. Them sinh vien moi\n";
         cout << "2. Hien thi danh sach sinh vien\n";
+        cout << "3. Tim kiem sinh vien\n";
+        cout << "4. Sap xep sinh theo diem trung binh cao den thap\n";
         cout << "0. Thoat\n";
         cout << "------------------------------------\n";
         cout << "Nhap lua chon cua ban: ";
@@ -43,6 +45,12 @@ int main() {
             case 2:
                 hienThiDanhSach(dsSinhVien);
                 break;
+            case 3:
+                timKiemSinhVien(dsSinhVien);
+                break;
+            case 4:
+                sapxep(dsSinhVien);
+                break;
             case 0:
                 cout << "Cam on ban da su dung chuong trinh!\n";
                 break;
@@ -54,10 +62,9 @@ int main() {
     return 0;
 }
 
-// Hàm nhập thông tin 1 sinh viên
 void nhapSinhVien(SinhVien &sv) {
     cout << "Nhap Ma Sinh Vien: ";
-    cin.ignore(); // Xóa bộ nhớ đệm
+    cin.ignore();
     getline(cin, sv.maSV);
     cout << "Nhap Ho va Ten: ";
     getline(cin, sv.hoTen);
@@ -72,7 +79,6 @@ void inSinhVien(const SinhVien &sv) {
          << setw(10) << fixed << setprecision(2) << sv.diemTB << endl;
 }
 
-// Hàm hiển thị toàn bộ danh sách
 void hienThiDanhSach(const vector<SinhVien> &ds) {
     if (ds.empty()) {
         cout << "\nDanh sach hien tai dang rong!\n";
@@ -86,4 +92,42 @@ void hienThiDanhSach(const vector<SinhVien> &ds) {
     for (const auto &sv : ds) {
         inSinhVien(sv);
     }
+}
+
+void timKiemSinhVien(const vector<SinhVien> &ds ){
+    if(ds.empty()){
+        cout<<"danh sach trong!\n";
+        return;
+    }
+    string id;
+    cout<<"Nhap ma sinh vien can tim: ";
+    cin>>id;
+    bool timthay= false;
+    for(int i=0;i<ds.size();i++){
+        if(id== ds[i].maSV){
+            cout<<"thong tin sinh vien co ma "<<ds[i].maSV<<" la: "<<ds[i].hoTen<<" voi diem trung binh: "<<ds[i].diemTB<<endl;
+            timthay=true;
+            break;
+        }
+    }
+    if(timthay!=true){
+        cout<<"\n KHONG TIM THAY SINH VIEN TUONG UNG!\n";
+    }
+}
+
+void sapxep(vector<SinhVien> &ds){
+    if(ds.empty()){
+        cout<<"danh sach trong!\n";
+        return;
+    }
+    SinhVien temp;
+    for(int i=0;i<ds.size()-1;i++){
+        for(int j=i+1;j<ds.size();j++)
+        if(ds[i].diemTB<ds[j].diemTB){
+            temp=ds[i];
+            ds[i]=ds[j];
+            ds[j]=temp;
+        }
+    }
+    hienThiDanhSach(ds);
 }
